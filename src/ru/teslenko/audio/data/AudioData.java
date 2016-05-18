bdf2a54d31f77c3e99fe06317ca370e835c78c5d
Teslenko Roman <TeslenkoRoman.rus56@gmail.com>
2016-05-18 23:05:00 +0500

    DFA-01-Implement_class_hierarchy_reading_audiofiles Реализация иерархии классов для чтения аудиофайлов

---
+++ src/ru/teslenko/audio/data/AudioData.java
@@ -0,0 +1,20 @@
+package ru.teslenko.audio.data;
+
+public class AudioData implements IAudioData {
+
+    @Override
+    public byte[] getData() {
+        return null;
+    }
+
+    @Override
+    public long getDataLength() {
+        return 0;
+    }
+
+    @Override
+    public double[] getSamples() {
+        return null;
+    }
+
+}
---
+++ src/ru/teslenko/audio/header/AudioHeader.java
@@ -0,0 +1,35 @@
+package ru.teslenko.audio.header;
+
+public class AudioHeader implements IAudioHeader {
+
+    @Override
+    public int getBitDepth() {
+        return 0;
+    }
+
+    @Override
+    public long getFramesCount() {
+        return 0;
+    }
+
+    @Override
+    public int getSampleSize() {
+        return 0;
+    }
+
+    @Override
+    public float getSampleRate() {
+        return 0;
+    }
+
+    @Override
+    public int getChannelsCount() {
+        return 0;
+    }
+
+    @Override
+    public double getDurationTime() {
+        return 0;
+    }
+
+}
