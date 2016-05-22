e1c55e8581d0609b93f22033ad3598db52fe0df4
Teslenko Roman <TeslenkoRoman.rus56@gmail.com>
2016-05-22 15:36:42 +0500

    DFA-04-Fast_Fourier_transform реализация интерфейса ITransform для дискретного преобразования Фурье

--- src/ru/teslenko/math_library/transform/DFT.java
+++ src/ru/teslenko/math_library/transform/DFT.java
@@ -1,23 +1,85 @@
 package ru.teslenko.math_library.transform;
 
-/** Дискретное преобразование Фурье */
-public class DFT {
+import ru.teslenko.math_library.complex.Complex;
 
-    /** Прямое дискретное преобразование Фурье */
-    public static Complex[] directDiscreteFourierTransform(double[] data) {
-        return directDiscreteFourierTransform(data, data.length);
+/** Дискретное преобразование Фурье */
+public class DFT implements ITransform {
+
+    @Override
+    public Complex[] transform(double[] data) {
+        return transform(data, data.length);
     }
 
-    /** Прямое дискретное преобразование Фурье */
-    public static Complex[] directDiscreteFourierTransform(double[] data, int lengthDFT) {
-        Complex[] complexData = new Complex[lengthDFT];
+    @Override
+    public Complex[] transform(double[] data, int lengthTransform) {
+        // data = 1024;
+        // lengthTransform = 1024;
+        // +
 
-        for (int k = 0; k < lengthDFT; k++) {
+        // data = 1024;
+        // lengthTransform = 2048;
+        // +
+
+        // data = 1024;
+        // lengthTransform = 512;
+        // +
+
+        // data = 1024;
+        // lengthTransform = 333;
+        // +
+
+        // data = 1024;
+        // lengthTransform = 2088;
+        // +
+
+        if (lengthTransform <= 0) {
+            System.out.println("Длина окна <= 0");
+            return new Complex[0];
+        }
+
+        // если длина массива меньше окна, то дополнить массив нулями
+        if (data.length < lengthTransform) {
+            double[] newComplexData = new double[lengthTransform];
+
+            for (int i = 0; i < data.length; i++) {
+                newComplexData[i] = data[i];
+            }
+            for (int i = data.length; i < lengthTransform; i++) {
+                newComplexData[i] = 0;
+            }
+
+            data = newComplexData;
+        }
+        // если длина массива больше окна, то обрезать массив
+        else if (data.length > lengthTransform) {
+            double[] newComplexData = new double[lengthTransform];
+
+            for (int i = 0; i < lengthTransform; i++) {
+                newComplexData[i] = data[i];
+            }
+
+            data = newComplexData;
+        }
+
+        return dft(data);
+    }
+
+    @Override
+    public Complex[] inverseTransform() {
+        return new Complex[0];
+    }
+
+    private Complex[] dft(double[] data) {
+        int lengthData = data.length;
+
+        Complex[] complexData = new Complex[lengthData];
+
+        for (int k = 0; k < lengthData; k++) {
 
             complexData[k] = new Complex();
-            for (int n = 0; n < lengthDFT; n++) {
+            for (int n = 0; n < lengthData; n++) {
                 // можно оптимизировать
-                double arg = 2 * Math.PI * k * n / lengthDFT;
+                double arg = 2 * Math.PI * k * n / lengthData;
 
                 complexData[k].addReal(data[n] * Math.cos(arg));
                 complexData[k].differenceImaginary(data[n] * Math.sin(arg));
@@ -27,5 +89,4 @@ public class DFT {
 
         return complexData;
     }
-
 }
